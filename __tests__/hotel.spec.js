e2ef1a830d5e756ee4c6012dc3b3f77726806027
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-04-12 22:36:46 +1000

    TEST - <Hotel /> + HotelInfo

---
+++ __tests__/hotel.spec.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { expect } from 'chai';
+
+import HotelInfo from '../app/hotel-info';
+
+describe('<Hotel />', function(){
+    const Hotel = require('../app/hotel.js');
+    const hotelProps = {
+        "id": "cxd650nuyo",
+        "title": "Courtyard by Marriott Sydney-North Ryde",
+        "address": "7-11 Talavera Rd, North Ryde",
+        "imageUrl": "https://unsplash.it/145/125/?random",
+        "rating": "4",
+        "ratingType": "self",
+        "promotion": "Exclusive Deal",
+        "room": {
+            "name": "Deluxe Balcony Room",
+            "price": "$329",
+            "currency": "AUD",
+            "savings": "$30~",
+            "pointsEarned": "0",
+            "freeCancellation": "true"
+        },
+    };
+
+    it('should render hotel info', function(){
+        const wrapper = shallow( <Hotel {...hotelProps} /> );
+        expect(wrapper.find(HotelInfo).exists()).to.be.true;
+    });
+
+
+});
