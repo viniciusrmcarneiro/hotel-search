745fef6970e2e9b1862d0c19fd0b985c6b479b7a
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-04-12 23:04:29 +1000

    refactoring - <Room />

--- app/room.js
+++ app/room.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import RoomInfo from './room-info';
 import RoomPrice from './room-price';
 
 const Room = ({
-    title, address, imageUrl, rating, ratingType,
+    title, pointsEarned, freeCancellation,
     price, currency, qtyNights, savings,
 })  => (
     <div>
+        <RoomInfo {...{title, pointsEarned, freeCancellation}}/>
         <RoomPrice {...{price, currency, qtyNights, savings,}}/>
     </div>
 );
@@ -15,16 +17,14 @@ const Room = ({
 Room.displayName = 'Room';
 
 Room.propTypes = {
-    title : PropTypes.string.isRequired,
-    address : PropTypes.string.isRequired,
-    imageUrl : PropTypes.string.isRequired,
-    rating : PropTypes.string.isRequired,
-    ratingType: PropTypes.oneOf(['self','star']).isRequired,
+    title : PropTypes.any.isRequired,
+    pointsEarned : PropTypes.any.isRequired,
+    freeCancellation : PropTypes.any,
 
-    price: PropTypes.string.isRequired,
-    currency: PropTypes.string.isRequired,
-    qtyNights: PropTypes.string.isRequired,
-    savings: PropTypes.string,
+    price: PropTypes.any.isRequired,
+    currency: PropTypes.any.isRequired,
+    qtyNights: PropTypes.any.isRequired,
+    savings: PropTypes.any,
 };
 
 module.exports = Room;
