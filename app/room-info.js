aef272592d975a9b143a92a56e9547980b20ea33
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-04-12 22:06:47 +1000

    IMPL - <RoomInfo /> free cancellation

--- app/room-info.js
+++ app/room-info.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RoomInfo = ({name, points_earned, })  => (
+const RoomInfo = ({name, points_earned, free_cancellation,})  => (
     <div>
         <span>{name}</span>
         <span>{points_earned}</span>
+        {free_cancellation && <span className="room-free-cancellation">Free cancellation</span>}
     </div>
 );
 
@@ -13,6 +14,7 @@ RoomInfo.displayName = 'RoomInfo';
 RoomInfo.propTypes = {
     name: PropTypes.string.isRequired,
     points_earned: PropTypes.string.isRequired,
+    free_cancellation: PropTypes.oneOfType([PropTypes.bool,PropTypes.string]),
 };
 
 module.exports = RoomInfo;
