8159602baef94d65efee0003808dce15c35023ef
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-04-12 20:58:10 +1000

    TEST - sort clicks <SearchHeader />

--- __tests__/search-header.spec.js
+++ __tests__/search-header.spec.js
@@ -1,14 +1,44 @@
 import React from 'react';
-
+import sinon from 'sinon';
 
 import { shallow } from 'enzyme';
 
-describe('SEARCH HEADER', function(){
-    const SearchHeader = require('../app/search-header.js');
-    const searchProps = { cityName: "Sydney", hotelsCount: 5, };
+import {expect} from 'chai';
 
-    it('renders the <SearchHeader />', function(){
+describe('<SearchHeader />', function(){
+    const SearchHeader = require('../app/search-header.js');
+    let sandbox = sinon.sandbox.create();;
+    let searchProps;
+
+
+    beforeEach(() => {
+        searchProps = {
+            cityName: "Sydney",
+            hotelsCount: 5,
+            sortOptions: {
+                "top-deals": "Top Deals",
+                "price-desc": "Price (high-low)",
+                "price-asc": "Price (low-high)",
+                "name-asc": "Name (A-Z)"
+            },
+            onSortChanged: sandbox.stub(),
+        };
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+
+    it('should render', function(){
         const wrapper = shallow( <SearchHeader {...searchProps} /> )
+    });
+
+    it('should call onSearchFilter when a Filter is selected', function(){
+        const wrapper = shallow( <SearchHeader {...searchProps} /> );
+        const eventObj = { target: { value : 'name-asc'} };
+        wrapper.find('#sort-input').simulate('change',  eventObj);
+        sinon.assert.calledWith(searchProps.onSortChanged, eventObj.target.value);
     });
 
 });
