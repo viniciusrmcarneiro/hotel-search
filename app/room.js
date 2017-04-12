ddf7259e680bfa55a0c5d44827c3ea7dc915379b
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-04-12 22:53:31 +1000

    IMPL - Room's price

--- app/room.js
+++ app/room.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import RoomPrice from './room-price';
 
-const Room = ({title, address, imageUrl, rating, ratingType})  => (
+const Room = ({
+    title, address, imageUrl, rating, ratingType,
+    price, currency, qtyNights, savings,
+})  => (
     <div>
+        <RoomPrice {...{price, currency, qtyNights, savings,}}/>
     </div>
 );
 
@@ -15,6 +20,11 @@ Room.propTypes = {
     imageUrl : PropTypes.string.isRequired,
     rating : PropTypes.string.isRequired,
     ratingType: PropTypes.oneOf(['self','star']).isRequired,
+
+    price: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+    qtyNights: PropTypes.string.isRequired,
+    savings: PropTypes.string,
 };
 
 module.exports = Room;
