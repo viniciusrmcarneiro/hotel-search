fbef012d4323b57bc11d1284b626787ecf0fcff9
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-04-12 21:00:20 +1000

    IMPLEMENTATION - sort clicks <SearchHeader />

--- app/search-header.js
+++ app/search-header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const SearchHeader = ({cityName, hotelsCount,})  => (
+const SearchHeader = ({cityName, hotelsCount, sortOptions, onSortChanged,})  => (
     <div>
         <span className="header-city-name">{cityName}</span>
         <span className="header-hotel-count">{hotelsCount}</span>
+        <select
+            id="sort-input"
+            onChange={evt => onSortChanged(evt.target.value)}>
+            {Object.keys(sortOptions).map( id => <option key={id} value={id}>{sortOptions[id]}</option>)}
+        </select>
     </div>
 );
 
