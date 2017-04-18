fc2ca8a8aedc68536ec3f5008296872dd74e8837
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-04-18 22:40:51 +1000

    IMPL - <HotelList />

---
+++ app/components/hotel-list.js
@@ -0,0 +1,38 @@
+import { connect } from 'react-redux'
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Hotel from './hotel';
+
+export const HotelList = ({waiting, hotels,})  => {
+    if (waiting === true){
+        return <span className="waiting">Loading hotels...</span>
+    }
+    
+    if (!hotels || hotels.length === 0){
+        return <span className="empty-list">there is not hotel</span>
+    }
+    return <div>
+        {hotels.map( props => <Hotel key={props.id} {...props}/>)}
+    </div>
+}
+
+HotelList.displayName = 'HotelList';
+
+HotelList.propTypes = {
+    waiting : PropTypes.bool,
+    hotels : PropTypes.array,
+};
+
+const mapStateToProps = state => {
+    console.log({state})
+    if (!state.hotelSearch || !state.hotelSearch.hotelsIds){
+        return {};
+    }
+
+    return {
+        hotels: state.hotelSearch.hotelsIds.map(id => state.hotelSearch.hotelsById[id])
+    };
+};
+
+export default connect(mapStateToProps)(HotelList)
