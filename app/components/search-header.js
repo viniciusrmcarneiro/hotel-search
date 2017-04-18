2b5b2fc14c5a93d6d4c50b213932bdc62e143beb
Vinícius Carneiro <vinicius.ribeiro.carneiro@gmail.com>
2017-04-18 23:40:40 +1000

    connections SearchHeader to the store

--- app/components/search-header.js
+++ app/components/search-header.js
@@ -1,8 +1,12 @@
-import { connect } from 'react-redux'
-
 import React from 'react';
 
 import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
+
+
+import { sortBy } from 'app/actions/hotel-search-actions';
 
 export const SearchHeader = ({cityName, hotelsCount, sortValue, sortOptions, onSortChanged,})  => {
     if (!cityName || !sortValue || !sortOptions || !onSortChanged){
@@ -27,7 +31,7 @@ SearchHeader.displayName = 'SearchHeader';
 
 SearchHeader.propTypes = {
     cityName: PropTypes.string,
-    hotelsCount: PropTypes.string,
+    hotelsCount: PropTypes.number,
     sortValue: PropTypes.string,
     sortOptions: PropTypes.shape({
         "top-deals": PropTypes.string.isRequired,
@@ -46,19 +50,15 @@ const mapStateToProps = state => {
         return {};
     }
 
-    if (state.hotelSearch.waiting){
-        return {
-            waiting: state.hotelSearch.waiting,
-        };
-    }
+    return Object.assign({},
+        state.hotelSearch.sort_filters ? { sortOptions: state.hotelSearch.sort_filters}      : null,
+        state.hotelSearch.sortedField  ? { sortValue: state.hotelSearch.sortedField}         : null,
+        state.hotelSearch.query        ? { cityName: state.hotelSearch.query.location }      : null,
+        state.hotelSearch.hotelsIds    ? { hotelsCount: state.hotelSearch.hotelsIds.length } : null,
+    );
 
-    if (!state.hotelSearch.hotelsIds){
-        return {};
-    }
-
-    return {
-        hotels: state.hotelSearch.hotelsIds.map(id => state.hotelSearch.hotelsById[id])
-    };
 };
 
-export default connect(mapStateToProps)(SearchHeader)
+const actions = (dispatch) => bindActionCreators({onSortChanged: sortBy},dispatch)
+
+export default connect(mapStateToProps, actions)(SearchHeader)
